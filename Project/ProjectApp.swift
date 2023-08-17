f57050904b61c6f18e78af0247c5f5fa6c3bcf49
eyhdev <alaqsa335@gmail.com>
2023-08-17 17:59:15 +0300

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by eyh.mac on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
